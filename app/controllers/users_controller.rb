c903329652f2d91c47dc9b5e33e93deea408f700
菊池真弘 <convenient60nls@gmail.com>
2017-05-03 12:58:38 +0900

    init commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  # def show
+   # @nickname = current_user.nickname
+
+   #変更前
+   # @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+
+   # @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+  # end
+
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+  end
+end
